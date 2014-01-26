510482bbcc0cc44aaee0f35e218d27661ea8b4a6
uwiga-feri <kurman488@yahoo.com>
2014-01-26 12:30:04 +0700

    Update TestGithub.java

--- src/testgithub/TestGithub.java
+++ src/testgithub/TestGithub.java
@@ -15,5 +15,6 @@ public class TestGithub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("tulisan yang keddua");
     }
 }
